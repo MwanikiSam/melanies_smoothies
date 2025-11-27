98e81633c4c63c1d2eedd4a5f7d620681c0fba14
Sam Mwaniki <smwaniki@gmail.com>
2025-11-27 07:35:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ if time_to_insert:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response)
-sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True
+sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
